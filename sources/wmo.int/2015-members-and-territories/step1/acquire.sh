855ebfc23ffd9f80e8b031b8a4d7597157d29edf
Eric Bréchemier <eric@bezen.org>
2015-01-26 18:09:29 +0100

    Add step 1: acquire data in HTML and JSON format

---
+++ sources/wmo.int/2015-members-and-territories/step1/acquire.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd $(dirname "$0")
+curl https://www.wmo.int/cpdb/data/membersandterritories \
+  > membersandterritories.html
+curl https://www.wmo.int/cpdb/data/membersandterritories.json \
+  > membersandterritories.json
