0de5554b957ec0887f22f67cb0dd5b3561d904af
Eric Bréchemier <eric@bezen.org>
2015-01-29 12:51:13 +0100

    Add step 1: acquire data in XML format from git submodule

---
+++ sources/debian.org/2014-iso-codes/step1/acquire.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd $(dirname "$0")
+currentPath=$(pwd)
+
+cd ../../../..
+git submodule update --init
+
+cd "$currentPath"
+cp -v iso-codes/iso_3166/iso_3166.xml .
+cp -v iso_3166.xml ..
