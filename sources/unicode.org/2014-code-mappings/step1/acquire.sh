f0060882b6e08f960960b0a657ddb6f613dc7ffa
Eric Bréchemier <eric@bezen.org>
2015-01-30 19:20:25 +0100

    Add step 1: acquire data in XML format and license in text format

---
+++ sources/unicode.org/2014-code-mappings/step1/acquire.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd $(dirname "$0")
+wget http://unicode.org/repos/cldr/trunk/unicode-license.txt
+wget http://unicode.org/repos/cldr/trunk/common/supplemental/supplementalData.xml
+cp supplementalData.xml ..
