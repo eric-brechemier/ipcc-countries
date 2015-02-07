2b3d3ea62c3d04b73a9f4d3b45f194fc86c82cf9
Eric Bréchemier <eric@bezen.org>
2015-02-07 18:23:50 +0100

    Add lists of countries from UN, Unicode, WMO in step 4: Refine

--- database/step4/refine.sql
+++ database/step4/refine.sql
@@ -53,3 +53,68 @@ OR SUBSTR( ipcc.Country, INSTR(ipcc.Country,', ') +2 )
 = debian.`Official Name`
 ORDER BY code, name
 ;
+
+.once un_country_names.csv
+SELECT DISTINCT
+  coalesce(
+    matcher.`ISO Country Code`,
+    debian.`Alpha-3 Country Code`,
+    'N/A'
+  ) code,
+  replace(un.`Member State`,char(0x200E),'') name
+FROM un_2015_members un
+LEFT JOIN wmo_2015_country_matcher matcher
+ON un.`Member State` = matcher.`Country Name To Identify`
+LEFT JOIN debian_2014_iso_codes debian
+ON un.`Member State` = debian.`Common Name`
+OR un.`Member State` = debian.Name
+OR un.`Member State` = debian.`Official Name`
+ORDER BY code, name
+;
+
+.once unicode_country_names.csv
+SELECT DISTINCT *
+FROM (
+  SELECT
+    map.`Alpha-3 ISO Country Code` code,
+    ter.`Territory Name` name
+  FROM unicode_2014_territories ter
+  JOIN unicode_2014_code_mappings map
+  ON ter.`Territory Code` = map.`Alpha-2 ISO Country Code`
+  -- ZZZ = 'Unknown Region'
+  WHERE map.`Alpha-3 ISO Country Code` NOT IN ('','ZZZ')
+  UNION
+  SELECT
+    map.`Alpha-3 ISO Country Code` code,
+    ter.`Territory Name` name
+  FROM unicode_2014_scripts_languages_territories ter
+  JOIN unicode_2014_code_mappings map
+  ON ter.`Territory Code` = map.`Alpha-2 ISO Country Code`
+  -- ZZZ = 'Unknown Region'
+  WHERE map.`Alpha-3 ISO Country Code` NOT IN ('','ZZZ')
+  UNION
+  SELECT
+    map.`Alpha-3 ISO Country Code` code,
+    ter.`Territory's Native Name` name
+  FROM unicode_2014_scripts_languages_territories ter
+  JOIN unicode_2014_code_mappings map
+  ON ter.`Territory Code` = map.`Alpha-2 ISO Country Code`
+  -- ZZZ = 'Unknown Region'
+  WHERE ter.`Territory's Native Name` <> 'n/a'
+  AND map.`Alpha-3 ISO Country Code` NOT IN ('','ZZZ')
+)
+ORDER BY code, name
+;
+
+.once wmo_country_names.csv
+SELECT DISTINCT *
+FROM (
+  SELECT `ISO Country Code` code, `English Country Name` name
+  FROM wmo_2015_members_territories
+  UNION
+  SELECT `ISO Country Code` code, `French Country Name` name
+  FROM wmo_2015_members_territories
+)
+ORDER BY code, name
+;
+
