7da8e1043b04a8b5d02d36c1aeadb9a8b02a22b4
Eric Bréchemier <eric@bezen.org>
2015-03-10 13:53:09 +0100

    Fix start line in meta.txt (1 after number of lines to skip)
    I also removed a redundant log message, which did not help
    to prevent this error.

--- sources/wikidata.org/2015-api-query-backlinks-to-iso-alpha3/step3/outsource-step2-parse.sh
+++ sources/wikidata.org/2015-api-query-backlinks-to-iso-alpha3/step3/outsource-step2-parse.sh
@@ -4,9 +4,9 @@ cd "$(dirname "$0")"
 
 xsltproc parse-meta.xsl ../step1/*.xml > meta.txt
 lines=$(wc -l meta.txt | cut -d' ' -f1)
-echo "Update $lines first lines of meta.txt:"
+start=$(expr $lines + 1)
 cat meta.txt
-tail -n "+$lines" ../meta.txt >> meta.txt
+tail -n "+$start" ../meta.txt >> meta.txt
 cp meta.txt ..
 
 xsltproc parse-data.xsl ../step1/*.xml > data.csv
