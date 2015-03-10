580abbc79790276e4689341a5778a11ba478846a
Eric Bréchemier <eric@bezen.org>
2015-03-10 17:43:56 +0100

    Make acquire.sh script for outsourcing more general

--- sources/wikidata.org/2015-api-query-backlinks-to-iso-alpha3/step3/outsource-step1-acquire.sh
+++ sources/wikidata.org/2015-api-query-backlinks-to-iso-alpha3/step3/outsource-step1-acquire.sh
@@ -3,7 +3,9 @@ cd "$(dirname "$0")"
 
 # read URL from meta.txt
 url=$(grep '^URL:' ../meta.txt | cut -d' ' -f2)
-file="${url##*/}"
+
+# read file name from meta.txt
+file=$(grep '^File:' ../meta.txt | cut -d' ' -f2)
 
 # read userAgent from property file
 . ./userAgent.property.sh
@@ -16,4 +18,4 @@ curl \
   --output "$file" \
   "$url"
 
-cp *.xml ..
+cp "$file" ..
