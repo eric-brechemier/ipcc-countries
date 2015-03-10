985885e3a30a9ed0b696d5966e1ada1812b7b62b
Eric Bréchemier <eric@bezen.org>
2015-03-10 17:46:45 +0100

    Make parse.sh script for outsourcing more general

--- sources/wikidata.org/2015-api-query-backlinks-to-iso-alpha3/step3/outsource-step2-parse.sh
+++ sources/wikidata.org/2015-api-query-backlinks-to-iso-alpha3/step3/outsource-step2-parse.sh
@@ -2,12 +2,15 @@
 # Requires: xsltproc
 cd "$(dirname "$0")"
 
-xsltproc parse-meta.xsl ../step1/*.xml > meta.txt
+# read file name from meta.txt
+file=$(grep '^File:' ../meta.txt | cut -d' ' -f2)
+
+xsltproc parse-meta.xsl "../step1/$file" > meta.txt
 lines=$(wc -l meta.txt | cut -d' ' -f1)
 start=$(expr $lines + 1)
 cat meta.txt
 tail -n "+$start" ../meta.txt >> meta.txt
 cp meta.txt ..
 
-xsltproc parse-data.xsl ../step1/*.xml > data.csv
+xsltproc parse-data.xsl "../step1/$file" > data.csv
 cp data.csv ..
