af373f825dcfeef057b6d9e9f54dc87d0b00709f
Eric Bréchemier <eric@bezen.org>
2015-04-07 10:48:02 +0200

    Combine multiple "outsourced" data.csv into a single "prepared" file
    At this step, I noticed lines which do not correspond to full records,
    probably field values with line breaks.
    
    In order to take care of CSV encoding issues in general, I have decided
    to introduce a separate transformation from XML (close to CSV in structure)
    to CSV (in actual text format). I will apply this transformation in all
    sources that generate CSV using XSLT.

--- database/step1/prepare.sh
+++ database/step1/prepare.sh
@@ -1,6 +1,33 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 
+# Combine data.csv files from a list of folders into a single file
+#
+# Parameters:
+#   $1 - string, the pattern which matches the set of folders
+#        which contain data.csv files
+#        Note: surround this parameter with single quotes
+#        to prevent wildcard expansion
+#   $2 - string, name of the output file
+combine()
+{
+  combine_isFirst="true"
+  for folder in $1
+  do
+    if test "$combine_isFirst" = "true"
+    then
+      # write the records including header,
+      # overwriting previous version of the file, if any
+      sed -f prepare.sed "$folder/data.csv" > "$2"
+      combine_isFirst="false"
+    else
+      # append the records without header
+      tail -n +2 "$folder/data.csv" |
+      sed -f prepare.sed >> "$2"
+    fi
+  done
+}
+
 # debian.org
 sed -f prepare.sed ../../sources/debian.org/2014-iso-codes/data.csv \
 > debian_2014_iso_codes.csv
@@ -29,11 +56,13 @@ sed -f prepare.sed ../../sources/un.org/2015-members-growth/data.csv \
 sed -f prepare.sed \
   ../../sources/wikidata.org/2015-api-query-backlinks-to-iso-alpha3/data.csv \
 > wikidata_2015_api_query_backlinks_to_iso_alpha3.csv
+combine '../../sources/wikidata.org/Q*' wikidata_entities.csv
 
 # wikimedia.org
 sed -f prepare.sed \
   ../../sources/wikimedia.org/2015-extinct-state-flags/data.csv \
 > wikimedia_2015_extinct_state_flags.csv
+combine '../../sources/wikimedia.org/*flag-of-*' wikimedia_flags.csv
 
 # wikipedia.org
 sed -f prepare.sed \
