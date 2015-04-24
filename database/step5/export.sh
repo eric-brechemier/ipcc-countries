026477c0b989d1226f178b95f4c3594ad4275d91
Eric Bréchemier <eric@bezen.org>
2015-04-24 10:30:59 +0200

    Export flags of IPCC countries to parent database/ folder

--- database/step5/export.sh
+++ database/step5/export.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # requires sqlite3 (3.8.8.2)
+# requires csvcut, from csvkit (0.9.0)
+
 cd "$(dirname "$0")"
 
 echo 'Export selected database views to CSV files'
@@ -10,3 +12,11 @@ cp *.csv ..
 ls ../*csv
 
 echo 'Copy IPCC country flags to parent data/ folder'
+rm -rf ../data
+mkdir ../data
+csvcut -c3 current_ipcc_members.csv |
+tail -n +2 | # skip header
+while read svgFilePath
+do
+  cp "../step2/$svgFilePath" "../$svgFilePath"
+done
