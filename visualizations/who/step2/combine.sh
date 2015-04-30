64c11181794ce2aab93bfba2eccbeec36d246ac3
Eric Bréchemier <eric@bezen.org>
2015-04-30 11:35:43 +0200

    Remove the copy of the list of current IPCC members from step 2
    Instead, I will now query the database to collect all the details
    needed for the list of countries in this visualization (illustrated
    with France):
    
    * first letter of country: F
    * link to Wikipedia: https://en.wikipedia.org/wiki/France
    * flag image: data/flag-of-france.svg
      or just 'flag-of-france' used as id in SVG file
    * common name in English: France
    * official name in English: French Republic
    * Alpha3 ISO country code: FRA

--- visualizations/who/step2/combine.sh
+++ visualizations/who/step2/combine.sh
@@ -3,9 +3,6 @@
 
 cd "$(dirname "$0")"
 
-echo 'Collect list of current IPCC members'
-cp ../../../database/current_ipcc_members.csv .
-
 echo 'Combine flags of current IPCC members into a single SVG file'
 ls ../../../database/data/*.svg |
 awk -f lines2xml.awk |
