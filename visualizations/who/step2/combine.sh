085849a1db2124af01cc2d89f8187658b2355ef2
Eric Bréchemier <eric@bezen.org>
2015-05-15 11:58:55 +0200

    Save XML list of flags as a file for reuse in CSS generation
    The same list will be the source for the list of CSS classes for each
    flag in the combined sprites image.

--- visualizations/who/step2/combine.sh
+++ visualizations/who/step2/combine.sh
@@ -3,8 +3,11 @@
 
 cd "$(dirname "$0")"
 
-echo 'Combine flags of current IPCC members into a single SVG file'
+echo 'List SVG file images as XML'
 ls ../../../database/data/*.svg |
-awk -f lines2xml.awk |
-xsltproc --novalid combine.xsl - \
+awk -f lines2xml.awk \
+> flags-list.xml
+
+echo 'Combine flags of current IPCC members into a single SVG file'
+xsltproc --novalid combine.xsl flags-list.xml \
 > flags.svg
