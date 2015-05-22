c62c57c77803b46335c50b712b5a67883bdac5d1
Eric Bréchemier <eric@bezen.org>
2015-05-22 20:15:28 +0200

    Define intermediate views to allow further calculations

--- visualizations/how/step1/query.sql
+++ visualizations/how/step1/query.sql
@@ -1,29 +1,29 @@
 .print 'Load database'
 .read ../../../database/database.sql
 
-.mode csv
-.headers on
+CREATE VIEW all_members
+AS
+SELECT DISTINCT iso3_code
+FROM (
+  SELECT iso3_code
+  FROM current_un_members
+  UNION
+  SELECT iso3_code
+  FROM current_wmo_members
+  UNION
+  SELECT iso3_code
+  FROM current_ipcc_members
+) every
+;
 
-.print 'Query the database to build the crosstabulation table'
-.once crosstab.csv
+CREATE VIEW cross_counts
+AS
 SELECT
   CASE WHEN un.iso3_code IS NULL THEN 'NOT UN' ELSE 'UN' END AS UN,
   IFNULL('WMO ' || wmo.state_or_territory, 'NOT WMO') AS WMO,
   CASE WHEN ipcc.iso3_code IS NULL THEN 'NOT IPCC' ELSE 'IPCC' END AS IPCC,
   COUNT(*) AS count
-FROM (
-  SELECT DISTINCT iso3_code
-  FROM (
-    SELECT iso3_code
-    FROM current_un_members
-    UNION
-    SELECT iso3_code
-    FROM current_wmo_members
-    UNION
-    SELECT iso3_code
-    FROM current_ipcc_members
-  ) every
-) each
+FROM all_members
 LEFT JOIN
 (
   SELECT iso3_code
@@ -43,5 +43,14 @@ LEFT JOIN
 ) ipcc
 USING (iso3_code)
 GROUP BY UN, WMO, IPCC
+;
+
+.mode csv
+.headers on
+
+.print 'Query the database to build the crosstabulation table'
+.once crosstab.csv
+SELECT *
+FROM cross_counts
 ORDER BY UN DESC, WMO, IPCC
 ;
